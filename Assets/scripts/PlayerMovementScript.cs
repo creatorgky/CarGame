31c546fa8a53bc6012db70f0522aa0387e9c0ef8
Gökay Benian <creatorgky@outlook.com>
2024-08-18 14:51:44 +0300

    Movment fixed

--- Assets/scripts/PlayerMovementScript.cs
+++ Assets/scripts/PlayerMovementScript.cs
@@ -7,11 +7,7 @@ public class PlayerMovementScript : MonoBehaviour
 {
     public float speed;
     private Vector2 move;
-
-    void Start()
-    {
-        
-    }
+    private Vector3 rotationTarget;
 
     void Update()
     {
@@ -23,14 +19,16 @@ public class PlayerMovementScript : MonoBehaviour
         move = context.ReadValue<Vector2>();
     }
 
-    public void movePlayer()
+    private void movePlayer()
     {
-        Vector3 movement = new Vector3(move.x, 0f, move.y);
+        Vector3 movement = new Vector3(-move.x, 0f,- move.y);
+
         if (movement != Vector3.zero)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement),0.15f);
+            rotationTarget = Quaternion.LookRotation(movement).eulerAngles;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotationTarget), 0.15f);
         }
+
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
-
 }
